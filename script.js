559181622f15b8a493cdce3f38e7d677ecc6d71a
IIDeveloperII <brayancampero5327@gmail.com>
2022-10-25 08:26:29 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function calculadora(){
-  alert("OPERACION SUMA\n\n Click en Aceptar o presiona Enter para continuar");
     let num1=parseInt(prompt("Ingrese el primer valor"));
     let num2=parseInt(prompt("Ingrese el segundo valor"));
-    if (opc==1){res=num1+num2}
+   if (opc==1){res=num1+num2}
     else if (opc==2){res=num1-num2  }
      else if (opc==3){res=num1*num2}
       else if (opc==4){res=num1/num2}
@@ -10,7 +9,6 @@ alert("El resultado es:  "+res);
 }
 let opc=parseInt(prompt("Ingrese la operacion a realizar\n 1. SUMA\n 2. RESTA\n 3. MULTIPLICACION\n 4. DIVISION"));
 calculadora();
-
 
 
 
