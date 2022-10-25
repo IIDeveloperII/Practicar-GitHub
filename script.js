5124bfe0998f9cb6e2b1520261cda017c84554b9
Brayan Florez Campero <brayancampero5327@gmail.com>
2022-10-25 06:51:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calculadora(){
-  alert("OPERACION SUMA\n\n Click en aceptar para continuar");
+  alert("OPERACION SUMA\n\n Click en Aceptar o presiona Enter para continuar");
     let num1=parseInt(prompt("Ingrese el primer valor"));
     let num2=parseInt(prompt("Ingrese el segundo valor"));
-    if (opc==1){ res=  num1+num2 }
-    else if (opc==2){res= num1-num2  }
-     else if (opc==3){ res= num1*num2}
-      else if (opc==4){res= num1/num2}
+    if (opc==1){res=num1+num2}
+    else if (opc==2){res=num1-num2  }
+     else if (opc==3){res=num1*num2}
+      else if (opc==4){res=num1/num2}
 alert("El resultado es:  "+res);
 }
 let opc=parseInt(prompt("Ingrese la operacion a realizar\n 1. SUMA\n 2. RESTA\n 3. MULTIPLICACION\n 4. DIVISION"));
