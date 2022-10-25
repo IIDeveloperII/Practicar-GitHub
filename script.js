a5aae0aba40452be7d2c11b8dcb3c846eeeb6340
Brayan Florez Campero <brayancampero5327@gmail.com>
2022-10-25 06:46:53 -0500

    Optimización de codigo

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
+function calculadora(){
+  alert("OPERACION SUMA\n\n Click en aceptar para continuar");
+    let num1=parseInt(prompt("Ingrese el primer valor"));
+    let num2=parseInt(prompt("Ingrese el segundo valor"));
+    if (opc==1){ res=  num1+num2 }
+    else if (opc==2){res= num1-num2  }
+     else if (opc==3){ res= num1*num2}
+      else if (opc==4){res= num1/num2}
+alert("El resultado es:  "+res);
+}
 let opc=parseInt(prompt("Ingrese la operacion a realizar\n 1. SUMA\n 2. RESTA\n 3. MULTIPLICACION\n 4. DIVISION"));
-if (opc==1) {
-    alert("OPERACION SUMA\n\n Click en aceptar para continuar");
-    let num1=parseInt(prompt("Ingrese el primer numero"));
-    let num2=parseInt(prompt("Ingrese el segundo numero"));
-    let res=num1+num2;
-    alert("El resultado es:  "+res);
-}
-else if (opc==2){
-     alert("OPERACION RESTA\n\n Click en aceptar para continuar");
-    let num1=parseInt(prompt("Ingrese el primer numero"));
-    let num2=parseInt(prompt("Ingrese el segundo numero"));
-    let res=num1-num2;
-    alert("El resultado es:  "+res); 
-}
-else if (opc==3){
-     alert("OPERACION MULTIPLICACION\n\n Click en aceptar para continuar");
-    let num1=parseInt(prompt("Ingrese el primer numero"));
-    let num2=parseInt(prompt("Ingrese el segundo numero"));
-    let res=num1*num2;
-    alert("El resultado es:  "+res); 
-}
-else if (opc==4){
-     alert("OPERACION DIVISION\n\n Click en aceptar para continuar");
-    let num1=parseInt(prompt("Ingrese el primer numero"));
-    let num2=parseInt(prompt("Ingrese el segundo numero"));
-    let res=num1*num2;
-    alert("El resultado es:  "+res); 
-}
-else{
-    alert("Operacion invalida");
-}
+calculadora();
 
 
 
